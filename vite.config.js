0e7b4c0a68289c17d77aeb64da5008fda6f5d375
Julia <113696424+YuliaPankiv@users.noreply.github.com>
2023-12-29 21:52:06 +0200

    add RTK

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pract-redux4/',
   server: {
     open: '/index.html',
     port: 8000,
