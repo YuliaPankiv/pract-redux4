a15a38ad0ade08bcee9bc06541e33e9bd5e0ba0f
Julia <113696424+YuliaPankiv@users.noreply.github.com>
2023-12-29 22:20:11 +0200

    add RTK

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/pract-redux4/',
+    base: '/pract-redux4/',
 
   plugins: [react()],
   // server: {
